5a5f940fdafe3a975025c232c7630c93727c4c15
Adam Weissman <adam@comedytragedyepic.com>
2019-09-25 08:16:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  def input_to_index(user_input) {user_input = gets.strip(print " ")}
+  def input_to_index(user_input) {user_input = gets.strip;end}
     print "Pick a number 1 - 9"
     yield
     user_input.to_i - 1
